11bd153cece1245b62a637a621be5f7687a0cde1
katherine <kmarmitage@gmail.com>
2019-05-05 18:24:32 -0700

    add firebase config

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    // Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyA4nYXQFK7plq6Kez0nyAcMXAPdGXZzfyY",
+        authDomain: "trainscheduler-6f148.firebaseapp.com",
+        databaseURL: "https://trainscheduler-6f148.firebaseio.com",
+        projectId: "trainscheduler-6f148",
+        storageBucket: "trainscheduler-6f148.appspot.com",
+        messagingSenderId: "627083201383",
+        appId: "1:627083201383:web:76d833fdcc69a8a0"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+});
