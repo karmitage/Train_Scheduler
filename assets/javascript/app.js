be67b0deff7ac532d539efea745de07ceb7d50bb
katherine <kmarmitage@gmail.com>
2019-05-05 18:39:57 -0700

    changed form IDs, added on click event listener for submit button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,5 +21,39 @@ $(document).ready(function () {
     var destination = '';
     var time = '';
     var frequency = 0;
+    var timeFormat = 'HHmm' //specifies formatting for military time
+
+    $("#search").on("click", function (event) {
+        // prevent page from refreshing when form tries to submit itself
+        event.preventDefault();
+
+        // Capture user inputs and store them in variables
+        name = $("#name").val().trim();
+        destination = $("#destination").val().trim();
+        time = $("#time").val().trim();
+        frequency = $("#frequency").val().trim();
+
+        // Console log each of the user inputs to confirm we are receiving them
+        console.log(name);
+        console.log(destination);
+        console.log(time);
+        console.log(frequency);
+
+        //this captures the "time" field input (which is a string) & stores it in 
+        //the format specified by the "timeFormat" variable and displays it in that format
+        //if we wanted, we could store it in one format and display it in another. but we're not going to do that.
+        time = moment(time, timeFormat).format(timeFormat);
+
+
+        // push data to our db
+        database.ref().push({
+            "name": name,
+            "destination": destination,
+            "time": time,
+            "frequency": frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
+    });
 
 });
